6b7e8aa2f716c5f2fa112b327a95b9e037413e81
Andrew Hayward <github@andrewhayward.net>
2014-07-23 17:14:13 +0100

    Extending team badges/members URLs

--- mozbadges/site/teams/urls.py
+++ mozbadges/site/teams/urls.py
@@ -8,7 +8,6 @@ urlpatterns = patterns('',
     (r'^teams', include(patterns('',
         # /teams.json
         url(r'^\.json$', views.team_list, name='json'),
-        url(r'^\.xml$', views.team_list, name='xml'),
 
         (r'^/', include(patterns('',
             # /teams/
@@ -16,16 +15,25 @@ urlpatterns = patterns('',
 
             (r'^(?P<team>[^ /.]+)', include(patterns('',
                 # /teams/{team}.json
-                url(r'\.json$', views.team_detail, name='json'),
+                url(r'^\.json$', views.team_detail, name='json'),
 
                 (r'^/', include(patterns('',
                     # /teams/{team}/
                     url(r'^$', views.team_detail, name='detail'),
 
-                    # /teams/{team}/badges/
-                    url(r'^badges/$', placeholder_view, name='badges'),
-                    # /teams/{team}/members/
-                    url(r'^members/$', placeholder_view, name='members'),
+                    (r'^badges', include(patterns('',
+                        # /teams/{team}/badges/
+                        url(r'^/$', placeholder_view, name='all'),
+                        # /teams/{team}/badges.json
+                        url(r'^\.json$', placeholder_view, name='json'),
+                    ), namespace='badges', app_name='badges')),
+
+                    (r'^members', include(patterns('',
+                        # /teams/{team}/members/
+                        url(r'^/$', placeholder_view, name='all'),
+                        # /teams/{team}/members.json
+                        url(r'^\.json$', placeholder_view, name='json'),
+                    ), namespace='members', app_name='members')),
 
                     # /teams/{team}/favorite/
                     url(r'^favorite/$', placeholder_view, name='favorite'),
