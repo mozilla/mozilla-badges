cfd5268fb7c95c8b39d5d8e047ad533c4b6aa2af
Andrew Hayward <github@andrewhayward.net>
2014-07-24 14:59:43 +0100

    Setting up some templates

--- mozbadges/utils/decorators.py
+++ mozbadges/utils/decorators.py
@@ -1,4 +1,5 @@
-from django.db.models import Field
+from django.shortcuts import render
+
 
 def public_attributes (*args, **kwargs):
     attributes = dict([(key, kwargs.get(key, key)) for key in (list(args) + kwargs.keys())])
@@ -8,4 +9,20 @@ def public_attributes (*args, **kwargs):
 
         return cls
 
+    return decorator
+
+
+def render_to(template_name):
+    def decorator(fn):
+        def view(request, *args, **kwargs):
+            response = fn(request, *args, **kwargs)
+
+            if response is None:
+                response = {}
+
+            if not isinstance(response, dict):
+                return response
+
+            return render(request, template_name, response)
+        return view
     return decorator
